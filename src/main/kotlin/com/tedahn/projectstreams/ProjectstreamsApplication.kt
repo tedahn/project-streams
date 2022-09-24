04bb8fe2ed03cd226c584edbe96fa42739315907
tedahn <@hn1996T>
2022-09-23 23:09:57 -0500

    init

---
+++ src/main/kotlin/com/tedahn/projectstreams/ProjectstreamsApplication.kt
@@ -0,0 +1,11 @@
+package com.tedahn.projectstreams
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ProjectstreamsApplication
+
+fun main(args: Array<String>) {
+	runApplication<ProjectstreamsApplication>(*args)
+}
---
+++ src/test/kotlin/com/tedahn/projectstreams/ProjectstreamsApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.tedahn.projectstreams
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ProjectstreamsApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
